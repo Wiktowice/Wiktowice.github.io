159aef7a31e9fbc840c36bb49b362af16da5bf2b
Wiktowice <Wiktowice@gmail.com>
2026-03-30 19:05:37 +0200

    Add Supabase configuration

---
+++ supabase_config.js
@@ -0,0 +1,7 @@
+// Supabase Configuration
+
+const SUPABASE_URL = 'https://qafdmnstcpnlulqjecei.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_iI_EEl7Zgd4gIFN__Fw6Tw_4ldFy_W-';
+
+// Initialize Supabase client
+var _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
